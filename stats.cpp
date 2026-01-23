4b8d89b8abc40d7cbdf0154030d70a72c6511579
Sandra Santhosh <102308614+aquiladive@users.noreply.github.com>
2026-01-23 23:22:21 +0530

    Stats type creation.

---
+++ stats.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+struct stat {
+    string name;
+    int value;
+};
+
+stat HP = {"HP", 0};
+stat MP = {"MP", 0};
+stat ATK = {"ATK", 0};
+stat MATK = {"MATK", 0};
+stat DEF = {"DEF", 0};
+stat MDEF = {"MDEF", 0};
+stat LUCK = {"LUCK", 0};
